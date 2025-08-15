e080d1e6b8a8a1ba5d5a12fd13fede0c2e717d2e
Gui Santos <gui.santos616@gmail.com>
2025-08-15 12:58:40 -0300

    port fix

--- webserver.py
+++ webserver.py
@@ -9,7 +9,7 @@ def home():
     return "Welcome to the Web Server!"
 
 def run_server():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)
 
 def keep_alive():
     thread = Thread(target=run_server)
